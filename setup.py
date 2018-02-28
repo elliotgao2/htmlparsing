516de15a77b7417ecb56bd28466afc31ef7dd030
p <97676212@qq.com>
2018-02-28 21:19:38 +0800

    release 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="htmlparsing",
-    version="0.1.1",
+    version="0.1.2",
     description="Pure HTML parsing library.",
     author="Jiuli Gao",
     author_email="gaojiuli@gmail.com",
@@ -13,6 +13,11 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+        'colorama',
+        'lxml',
+        'html2text'
     ],
     packages=find_packages(),
     include_package_data=True,
