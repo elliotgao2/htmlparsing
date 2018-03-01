d6e6194a97dc5f6f1d85f3bf9b1fbb6444996c85
Jiuli Gao <gaojiuli@gmail.com>
2018-03-01 18:03:38 +0800

    Update

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='htmlparsing',
+    version='0.1.1',
+    packages=['htmlparsing'],
+    url='https://github.com/gaojiuli/htmlparsing',
+    license='MIT',
+    description="Pure HTML parsing library.",
+    author="Jiuli Gao",
+    author_email="gaojiuli@gmail.com",
+    include_package_data=True,
+    zip_safe=False,
+)
